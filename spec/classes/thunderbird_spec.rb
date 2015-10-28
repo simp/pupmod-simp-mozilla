7e0a1b831e999f6cc53545029d4a98ab824d17e8
Dana Parker <dwatkins-parker@keywcorp.com>
2015-10-28 17:32:04 +0000

    (SIMP-577) Add Acceptance Test
    Adding Acceptance test; updating spec test

--- spec/classes/thunderbird_spec.rb
+++ spec/classes/thunderbird_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe 'mozilla::thunderbird' do
+  context 'supported operating system' do
+    on_supported_os.each do |os, facts|
+      let(:facts) {{ :hardwaremodel => 'x86_64' }}
 
-  let(:facts) {{ :hardwaremodel => 'x86_64' }}
+  context "on #{os}" do
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to create_class('mozilla::thunderbird') }
 
-  it { should create_class('mozilla::thunderbird') }
-  it { should contain_package('thunderbird.x86_64') }
+    context 'base'do
+      it { is_expected.to contain_package('thunderbird.x86_64').with(:ensure =>'latest') }
+    end
+   end
+  end
+ end
 end
+
