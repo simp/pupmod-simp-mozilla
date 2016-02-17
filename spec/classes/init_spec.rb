eac6a222693a335d4f105b664552048339833399
nick-markowski <nmarkowski@keywcorp.com>
2016-02-17 19:08:15 +0000

    (SIMP-577) Added acceptance tests
    Added acceptance and updated spec testing.
    
    SIMP-577 #close
    Change-Id: I63d317bd4e017d0efaf60a888c02453501cabb57

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,17 +1,15 @@
 require 'spec_helper'
 
-describe 'mozilla::firefox' do
+describe 'mozilla' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
-
       let(:facts) do
         facts
       end
 
-      context "on #{os}"do
+      context "on #{os}" do
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_class('mozilla::firefox') }
-        it { is_expected.to contain_package('firefox.x86_64') }
+        it { is_expected.to create_class('mozilla') }
       end
     end
   end
